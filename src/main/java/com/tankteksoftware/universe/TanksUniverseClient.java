bb0f4d9e0992b9bb70a85676b9c25659ed24551d
msmith-codes <msmith.softwaredev@gmail.com>
2025-10-30 20:53:43 -0400

    Updated project files

---
+++ src/main/java/com/tankteksoftware/universe/TanksUniverseClient.java
@@ -0,0 +1,14 @@
+package com.tankteksoftware.universe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TanksUniverseClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		
+	}
+
+	
+	
+}
